b1562b008c90edad5fc238615caeb196039f4d93
xstefank <xstefank122@gmail.com>
2021-12-02 19:32:33 +0100

    Advent 1/2

--- src/main/java/io/xstefank/AdventService.java
+++ src/main/java/io/xstefank/AdventService.java
@@ -6,7 +6,9 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.io.IOException;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
 
 @Path("/")
 public class AdventService {
@@ -16,8 +18,8 @@ public class AdventService {
 
     @GET
     @Path("/1/1")
-    public void migratoryBirdsREST() throws IOException {
-        Scanner scanner = new Scanner(inputReader.getFile("1-1.txt"));
+    public void advent11() throws IOException {
+        Scanner scanner = new Scanner(inputReader.getFile("1.txt"));
 
         long increaseSum = 0;
 
@@ -34,6 +36,48 @@ public class AdventService {
         }
 
         System.out.println("Increase sum = " + increaseSum);
+    }
+
+    @GET
+    @Path("/1/2")
+    public void advent12() throws IOException {
+        Scanner scanner = new Scanner(inputReader.getFile("1.txt"));
+
+        int windowSize = 3;
+
+        long increaseWindowSum = 0;
+
+        Queue<Integer> queue = new ArrayBlockingQueue<>(windowSize);
+
+        int toAdd;
+        int currentSum = scanner.nextInt();
+        queue.add(currentSum);
+
+        for (int i = 1; i < windowSize; i++) {
+            toAdd = scanner.nextInt();
+            queue.add(toAdd);
+            currentSum += toAdd;
+        }
+
+        int previousSum = currentSum;
+
+        while (scanner.hasNext()) {
+            toAdd = scanner.nextInt();
+
+            currentSum -= queue.poll();
+            currentSum += toAdd;
+
+            if (currentSum > previousSum) {
+                increaseWindowSum++;
+            }
+
+
+            queue.add(toAdd);
+            previousSum = currentSum;
+
+        }
+
+        System.out.println("Increase sum = " + increaseWindowSum);
     }
 
 }

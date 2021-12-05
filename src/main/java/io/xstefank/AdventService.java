26cdc6097c4bc47a690a221f5e79b550bca07851
xstefank <xstefank122@gmail.com>
2021-12-05 15:47:48 +0100

    Advent 4/1

--- src/main/java/io/xstefank/AdventService.java
+++ src/main/java/io/xstefank/AdventService.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Collectors;
 
 @Path("/")
 public class AdventService {
@@ -174,7 +176,6 @@ public class AdventService {
         Scanner scanner = new Scanner(inputReader.getFile("3.txt"));
 
         List<String> numbers = new ArrayList<>();
-        String current;
 
         while (scanner.hasNext()) {
             numbers.add(scanner.next());
@@ -238,4 +239,97 @@ public class AdventService {
         System.out.println(Long.parseLong(oxygenNumbers.get(0), 2) * Long.parseLong(co2Numbers.get(0), 2));
     }
 
+    @GET
+    @Path("/4/1")
+    public void advent41() throws IOException {
+        Scanner scanner = new Scanner(inputReader.getFile("4.txt"));
+
+        String numbersLine = scanner.nextLine();
+        Queue<Integer> toCallNumbers = new ArrayDeque<>(Arrays.stream(numbersLine.split(",")).map(Integer::parseInt).toList());
+
+        int boardSize = 5;
+        List<Board> boards = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            Board board = new Board();
+            for (int i = 0; i < boardSize; i++) {
+                for (int j = 0; j < boardSize; j++) {
+                    board.board[i][j] = scanner.nextInt();
+                }
+            }
+            boards.add(board);
+        }
+
+        boolean winHit = false;
+        int playedNumber = 0;
+        Board currentBoard = null;
+
+        while (!winHit) {
+            playedNumber = toCallNumbers.poll();
+
+            for (Board b : boards) {
+                currentBoard = b;
+                winHit = b.play(playedNumber);
+                if (winHit) {
+                    break;
+                }
+            }
+        }
+
+        int sumOfUnmarked = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!currentBoard.marks[i][j]) {
+                    sumOfUnmarked += currentBoard.board[i][j];
+                }
+            }
+        }
+
+        System.out.println(sumOfUnmarked * playedNumber);
+
+    }
+
+    private class Board {
+        public int[][] board = new int[5][5];
+        public boolean[][] marks = new boolean[5][5];
+
+        public boolean play(int playedNumber) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (board[i][j] == playedNumber) {
+                        marks[i][j] = true;
+                        if (checkWin(i, j)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private boolean checkWin(int row, int column) {
+            // check if row is winning
+            boolean rowWinning = true;
+            for (int j = 0; j < 5; j++) {
+                rowWinning = rowWinning && marks[row][j];
+            }
+
+            if (rowWinning) {
+                return true;
+            }
+
+            // check if column is winning
+            boolean columnWinning = true;
+            for (int i = 0; i < 5; i++) {
+                columnWinning = columnWinning && marks[i][column];
+            }
+
+            if (columnWinning) {
+                return true;
+            }
+
+            return false;
+        }
+    }
 }

dcbd0c30a7c08aeabcaa78d663f8e6ed81c871c1
xstefank <xstefank122@gmail.com>
2021-12-17 20:47:59 +0100

    Advent 14/2

--- src/main/java/io/xstefank/AdventService.java
+++ src/main/java/io/xstefank/AdventService.java
@@ -764,7 +764,7 @@ public class AdventService {
     }
 
     private boolean isSubSequence(String subsequence, String signal) {
-        for (char c: subsequence.toCharArray()) {
+        for (char c : subsequence.toCharArray()) {
             if (signal.indexOf(c) < 0) {
                 return false;
             }
@@ -1554,6 +1554,74 @@ public class AdventService {
 
 
         for (Integer occurrence : occurrences.values()) {
+            if (occurrence > max) {
+                max = occurrence;
+            }
+            if (occurrence < min) {
+                min = occurrence;
+            }
+        }
+
+        System.out.println(max - min);
+    }
+
+    @GET
+    @Path("/14/2")
+    public void advent142() throws IOException {
+        Scanner scanner = new Scanner(inputReader.getFile("14.txt"));
+
+        String template = scanner.nextLine();
+
+        //empty line after that
+        scanner.nextLine();
+
+        Map<String, String[]> pairInsertions = new HashMap<>();
+        String line;
+        String[] split;
+
+        while (scanner.hasNext()) {
+            line = scanner.nextLine();
+            split = line.split(" -> ");
+            pairInsertions.put(split[0], new String[]{split[0].charAt(0) + split[1], split[1] + split[0].charAt(1)});
+        }
+
+        Map<Character, Long> occurrences = new HashMap<>();
+
+        for (char c : template.toCharArray()) {
+            occurrences.merge(c, 1L, Long::sum);
+        }
+
+        Map<String, Long> pairCounts = new HashMap<>();
+        Map<String, Long> newPairCounts;
+
+        for (int i = 0; i < template.length() - 1; i++) {
+            pairCounts.merge(template.substring(i, i + 2), 1L, Long::sum);
+        }
+
+        System.out.println(pairCounts);
+
+        for (int i = 0; i < 40; i++) {
+            newPairCounts = new HashMap<>();
+
+            for (Map.Entry<String, Long> pairCount : pairCounts.entrySet()) {
+                String[] newPairs = pairInsertions.get(pairCount.getKey());
+                occurrences.merge(newPairs[0].charAt(1), pairCount.getValue(), Long::sum);
+
+                Long count1 = newPairCounts.getOrDefault(newPairs[0], 0L);
+                Long count2 = newPairCounts.getOrDefault(newPairs[1], 0L);
+
+                newPairCounts.put(newPairs[0], count1 + pairCount.getValue());
+                newPairCounts.put(newPairs[1], count2 + pairCount.getValue());
+            }
+
+            pairCounts = newPairCounts;
+        }
+
+        long max = 0;
+        long min = Long.MAX_VALUE;
+
+
+        for (Long occurrence : occurrences.values()) {
             if (occurrence > max) {
                 max = occurrence;
             }
